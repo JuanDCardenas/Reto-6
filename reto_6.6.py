6b66ef99c85ff93ddf136d8ba12ded1dedeb1d11
JuanDCardenas <159057545+JuanDCardenas@users.noreply.github.com>
2024-03-23 04:48:19 -0500

    Update reto_6.6.py

--- reto_6.6.py
+++ reto_6.6.py
@@ -1,6 +1,7 @@
 def contagiados (dias_transcurridos:float,contagiados_iniciales:float)->float:
     return contagiados_iniciales*(2**dias_transcurridos)
-contagiados_iniciales=float(input("Escriba el numero de contagiados que habia en un principio: "))
-dias_transcurridos=float(input("Escriba cuantos dias han transcurrido: "))
-contagiados_finales=contagiados(dias_transcurridos,contagiados_iniciales)
-print(f"El numero de contagiados tras {dias_transcurridos} dias de que hubieran {contagiados_iniciales} contagiados es de {contagiados_finales}")
+if __name__ == "__main__":
+    contagiados_iniciales=float(input("Escriba el numero de contagiados que habia en un principio: "))
+    dias_transcurridos=float(input("Escriba cuantos dias han transcurrido: "))
+    contagiados_finales=contagiados(dias_transcurridos,contagiados_iniciales)
+    print(f"El numero de contagiados tras {dias_transcurridos} dias de que hubieran {contagiados_iniciales} contagiados es de {contagiados_finales}")
